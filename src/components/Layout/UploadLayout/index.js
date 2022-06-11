211aef75d043b803054fc095a20c954581c451d7
unknown <longhoang1999>
2022-06-11 14:51:52 +0700

    updaye

---
+++ src/components/Layout/UploadLayout/index.js
@@ -0,0 +1,12 @@
+function UploadLayout({ children }) {
+    return (
+        <div>
+            <div className="container">
+                Layout riêng cho Upload
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default UploadLayout;
---
+++ src/routers/index.js
@@ -0,0 +1,18 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+
+import UploadLayout from '~/components/Layout/UploadLayout';
+
+const publicRouter = [
+    // router không cần đăng nhập vẫn vào được
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: null },
+    { path: '/upload-header', component: Upload, layout: UploadLayout },
+];
+const privateRouter = [
+    // router phải đăng nhập mới vào được
+];
+
+export { publicRouter, privateRouter };
